9ddc8796c1916b507df34ef2ba9dd2a0838818d9
royer <i2226824@continental.edu.pe>
2023-03-05 00:06:06 -0500

    Añadir la función para eliminar rooms Fixes #1

--- src/controllers/room.controller.ts
+++ src/controllers/room.controller.ts
@@ -51,5 +51,8 @@ export const updateRoom = async (req: Request, res: Response) => {
 }
 
 export const deleteRoom = async (req: Request, res: Response) => {
+  const conn = connect();
+  const { id } = req.params;
+  await conn.query('DELETE FROM room WHERE id=?',[id]);
   res.send("DELETE ROOM")
 }
