80c488159d184ac34173229b3802a67e2850aa42
Jhonatan <jhoonatan_0912@hotmail.com>
2023-06-03 23:44:07 -0500

    variables updated

--- src/database.ts
+++ src/database.ts
@@ -2,11 +2,11 @@ import { createPool } from "mysql2/promise";
 
 export const connect = () => {
   const connection = createPool({
-    host: 'localhost',
+    host: 'containers-us-west-84.railway.app',
     user: 'root',
-    password: '1234',
-    database: 'hotel_torre_torre',
-    port: 3306,
+    password: 'lGJHNN5WaNdGRaQK5oeP',
+    database: 'railway',
+    port: 7857,
     connectionLimit: undefined
   })
   return connection;
